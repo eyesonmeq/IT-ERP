242c08b7c3cc36baca68764546ee32b32fdfb831
eyesonmeq <liming820829@163.com>
2017-12-02 17:28:19 +0800

    Tony

---
+++ modules/erp-common/src/main/java/com/angel/erp/common/constant/ConfigConstant.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) 2017, All Rights Reserved. 
+ */
+package com.angel.erp.common.constant;
+
+/**
+ * ADD FUNCTION
+ *
+ * @date: 2017年12月2日 下午5:14:00
+ * @author li_ming 
+ */
+public class ConfigConstant {
+
+}
